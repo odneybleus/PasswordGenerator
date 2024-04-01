fe6b8ebf5730723c09a389bef369fc3085bd24dc
Odney Bleus <odney@Odneys-Air.lan>
2024-04-01 00:59:25 -0600

    added random letters function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+var letter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var randomletters = function(freq){
+  var Capletter = "";
+ for(let i = 0; i < freq; i++){
+  const random = Math.floor(Math.random() * Capletter.length )
+  Capletter += letter[random]
+ }
+ return Capletter
+}
+var PasswordLength = function(n){
+if(n >= 8 || n < 128) {
+  return n
+}
+}
 
+
+var generatePassword = function (){
+window.alert(`Welcome to password Generator!`)
+ var promptLength = window.prompt(`What is the length of the password? Password has to be minium 8 characters no more then 128` )
+ if(promptLength >= 8 || promptLength < 128){
+  return randomletters(promptLength);
+ }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
