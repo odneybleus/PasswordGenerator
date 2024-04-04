a84b37b4ea2c9bd1f0c41e622fda6ce6e45319c0
Odney Bleus <odney@Odneys-Air.lan>
2024-04-04 06:23:37 -0600

    able to generate random capital letters

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,56 @@
 // Assignment code here
-var letter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var randomletters = function(freq){
-  var Capletter = "";
- for(let i = 0; i < freq; i++){
-  const random = Math.floor(Math.random() * Capletter.length )
-  Capletter += letter[random]
- }
- return Capletter
-}
-var PasswordLength = function(n){
-if(n >= 8 || n < 128) {
-  return n
-}
-}
+var allCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var allLower ="abcdefghijklmnopqrstuvwxyz"
+var allSpec = "!@#$%^&*()+-?<>"
+var allNum = "1234567890";
+
+
+// var randomUpperCaseLetters = function(freq){
+//   var Capletter = "";
+//  for(let i = 0; i < freq; i++){
+//   const random = Math.floor(Math.random() * Capletter.length )
+//   Capletter += upperCase[random]
+//  }
+//  return Capletter
+// }
+
+
+
+// var randomLoweCaseLetters = function(freq){
+//   var Capletter = "";
+//  for(let i = 0; i < freq; i++){
+//   const random = Math.floor(Math.random() * Capletter.length )
+//   Capletter += lowerCase[random]
+//  }
+//  return Capletter
+// }
+
 
 
 var generatePassword = function (){
+  var passString = ""
 window.alert(`Welcome to password Generator!`)
  var promptLength = window.prompt(`What is the length of the password? Password has to be minium 8 characters no more then 128` )
- if(promptLength >= 8 || promptLength < 128){
-  return randomletters(promptLength);
- }
-}
+ var promptlowercase =window.confirm(`Would you like to add lower case letters to password?`)
+ var promptCapitalcase =window.confirm(`Would you like to add capital case letters to password?`)
+ while(promptLength >= 8 && promptLength < 128){
+ if(promptCapitalcase){
+  for(let i = 0; i < promptLength; i++){
+    const random = Math.floor(Math.random() * allCap.length)
+    passString += allCap.charAt(random)
+  }
+   return passString
+ } 
+//  if(promptlowercase){
+//   for(let i = 0; i < promptLength; i++){
+//     const random = Math.floor(Math.random() * passString.length)
+//     passString += allLower[random]
+//   }
+//    return passString
+//  }
 
+}
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
